9381610d4577b0407eedcf80f267d0f02d4f0757
Leonard Göhrs <l.goehrs@pengutronix.de>
2023-04-06 12:18:15 +0200

    meta-lxatac-software: bundles: hook.sh: enable the cert signing this bundle
    This makes it so that an image installed via rauc will trust only the key the
    bundle was signed with.
    
    This will be used to support our official bundle release channels, where
    bundles will be re-signed multiple times in the release process.
    
    First the bundles are built and signed with the rauc-devel.key.pem key and
    tested on hardware.
    If the tests are passed the existing bundle will be automatically re-signed
    with the nightly release key.
    If a new stable release is to be released an existing nightly bundle that has
    been manually tested is manually re-signed with the stable release key.
    
    All of these bundles are deployed with the same /etc/rauc/certificates-*
    but a stable bundle should only be able to install other stable bundles,
    so we need to detect the release channel at install-time.
    
    This commit makes it so that only the certificate matching the key the bundle
    was signed with is enabled.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- meta-lxatac-software/recipes-core/bundles/files/hook.sh
+++ meta-lxatac-software/recipes-core/bundles/files/hook.sh
@@ -2,6 +2,39 @@
 
 set -exu -o pipefail
 
+CERT_AVAILABLE_DIR="${RAUC_SLOT_MOUNT_POINT}/etc/rauc/certificates-available"
+CERT_ENABLED_DIR="${RAUC_SLOT_MOUNT_POINT}/etc/rauc/certificates-enabled"
+
+function enable_certificates () {
+	# Ignore the enabled certifcates from the bundle
+	rm "${CERT_ENABLED_DIR}"/*
+
+	for cert in "${CERT_AVAILABLE_DIR}"/*; do
+		cert_name=$(basename "${cert}")
+
+		cert_hash=$(openssl x509 -pubkey -noout -in "${cert}" \
+			| openssl pkey -pubin -outform der \
+			| openssl dgst -sha256 -c -hex \
+			| awk '{print toupper($2)}')
+
+		# Enable certificates that match the hash of the public key(s)
+		# that the current bundle is signed with.
+		# This means that a bundle signed with e.g. an official stable
+		# channel certificate will only be able to install other
+		# bundles from the same release channel.
+		for bundle_hash in ${RAUC_BUNDLE_SPKI_HASHES}; do
+			if [ "${bundle_hash}" == "${cert_hash}" ]; then
+				echo "Enable certificate ${cert_name}"
+				ln -s \
+				   "../certificates-available/${cert_name}"\
+				   "${CERT_ENABLED_DIR}/${cert_name}"
+			fi
+		done
+	done
+
+	openssl rehash "${CERT_ENABLED_DIR}"
+}
+
 function migrate () {
 	if [ ! -f "$1" ]; then
 		return
@@ -13,6 +46,7 @@ function migrate () {
 
 case "$1" in
 	slot-post-install)
+		enable_certificates
 		migrate /etc/machine-id
 		migrate /etc/labgrid/environment
 		migrate /etc/labgrid/userconfig.yaml
