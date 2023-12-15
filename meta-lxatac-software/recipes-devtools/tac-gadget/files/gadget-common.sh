4f825d92e685e5695c29dc8ee35cb55a404a64f2
Leonard Göhrs <l.goehrs@pengutronix.de>
2023-12-15 15:51:48 +0100

    meta-lxatac-software: tac-gadget: fix shellcheck warnings in helper scripts
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- meta-lxatac-software/recipes-devtools/tac-gadget/files/gadget-common.sh
+++ meta-lxatac-software/recipes-devtools/tac-gadget/files/gadget-common.sh
@@ -13,29 +13,29 @@ PRODUCTNAME="LXATAC"
 UDC_ADDR="49000000.usb-otg"
 
 clear_gadget () {
-    if [ -s $DEVDIR/UDC ]; then
+    if [[ -s "${DEVDIR}/UDC" ]]; then
         echo "USB Gadget is already set up."
         exit 11
-    elif [ -s $MAINDIR/*/UDC ]; then
+    elif [[ -s "${MAINDIR}"/*/UDC ]]; then
         echo "Remove existing USB Gadgets."
         # when removing a gadget we have to reverse the init process
-        for dir in $MAINDIR/*/configs/*/strings/*; do
-            [ -d $dir ] && rmdir $dir
+        for dir in "${MAINDIR}"/*/configs/*/strings/*; do
+            test -d "${dir}" && rmdir "${dir}"
         done
-        for func in $MAINDIR/*/configs/*.*/*.*; do
-            [ -e $func ] && rm $func
+        for func in "${MAINDIR}"/*/configs/*.*/*.*; do
+            test -e "${func}" && rm "${func}"
         done
-        for conf in $MAINDIR/*/configs/*; do
-            [ -d $conf ] && rmdir $conf
+        for conf in "${MAINDIR}"/*/configs/*; do
+            test -d "${conf}" && rmdir "${conf}"
         done
-        for func in $MAINDIR/*/functions/*.*; do
-            [ -d $func ] && rmdir $func
+        for func in "${MAINDIR}"/*/functions/*.*; do
+            test -d "${func}" && rmdir "${func}"
         done
-        for str in $MAINDIR/*/strings/*; do
-            [ -d $str ] && rmdir $str
+        for str in "${MAINDIR}"/*/strings/*; do
+            test -d "${str}" && rmdir "${str}"
         done
-        rmdir $MAINDIR/*
-    elif [ -n $DEVDIR ]; then
+        rmdir "${MAINDIR}"/*
+    elif [[ -n "${DEVDIR}" ]]; then
         modprobe libcomposite
     else
         echo "Nothing to do here."
@@ -45,20 +45,20 @@ clear_gadget () {
 
 setup_gadget () {
     echo "Set up new USB Gadget."
-    mkdir $DEVDIR
-    echo $VENDOR > $DEVDIR/idVendor
-    echo $PRODUCT > $DEVDIR/idProduct
+    mkdir "${DEVDIR}"
+    echo "${VENDOR}" > "${DEVDIR}/idVendor"
+    echo "${PRODUCT}" > "${DEVDIR}/idProduct"
 
-    mkdir $DEVDIR/strings/0x409 # set language to EN-US
-    echo $SERIAL > $DEVDIR/strings/0x409/serialnumber
-    echo $VENDORNAME > $DEVDIR/strings/0x409/manufacturer
-    echo $PRODUCTNAME > $DEVDIR/strings/0x409/product
+    mkdir "${DEVDIR}/strings/0x409" # set language to EN-US
+    echo "${SERIAL:?}" > "${DEVDIR}/strings/0x409/serialnumber"
+    echo "${VENDORNAME}" > "${DEVDIR}/strings/0x409/manufacturer"
+    echo "${PRODUCTNAME}" > "${DEVDIR}/strings/0x409/product"
 
-    mkdir $DEVDIR/configs/c.1
-    mkdir $DEVDIR/configs/c.1/strings/0x409
-    echo Normal > $DEVDIR/configs/c.1/strings/0x409/configuration
+    mkdir "${DEVDIR}/configs/c.1"
+    mkdir "${DEVDIR}/configs/c.1/strings/0x409"
+    echo Normal > "${DEVDIR}/configs/c.1/strings/0x409/configuration"
 }
 
 start_gadget () {
-    echo $UDC_ADDR > $DEVDIR/UDC
+    echo "${UDC_ADDR}" > "${DEVDIR}/UDC"
 }
